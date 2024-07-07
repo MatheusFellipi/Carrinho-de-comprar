104bd5a69210c87b3288e4c810d1cb924a36b04b
Matheus Fellipi <matheus.fellipi@outlook.com>
2024-07-07 10:31:41 -0300

    feat: add items in pages

--- src/components/cart/list.tsx
+++ src/components/cart/list.tsx
@@ -1,42 +1,112 @@
-import * as React from "react";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
 import ListItemAvatar from "@mui/material/ListItemAvatar";
 import Avatar from "@mui/material/Avatar";
-import ImageIcon from "@mui/icons-material/Image";
-import WorkIcon from "@mui/icons-material/Work";
-import BeachAccessIcon from "@mui/icons-material/BeachAccess";
-
-const img = [1, 2, 3, 4, 5, 6, 7];
+import DeleteIcon from "@mui/icons-material/Delete";
+import ListItemButton from "@mui/material/ListItemButton";
+import styles from "./styles.module.css";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useCart } from "@/shared/hook/cart";
+import {
+  Box,
+  IconButton,
+  ListItemText,
+  Typography,
+  useMediaQuery,
+} from "@mui/material";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 export default function ListProductsComponent() {
+  const matches = useMediaQuery("(max-width:767px)");
+  const { cart, handleRemover, handleQtdAdd, handleQtdRemove } = useCart();
+
   return (
-    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <ImageIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Photos" secondary="Jan 9, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <WorkIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Work" secondary="Jan 7, 2014" />
-      </ListItem>
-      <ListItem>
-        <ListItemAvatar>
-          <Avatar>
-            <BeachAccessIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Vacation" secondary="July 20, 2014" />
-      </ListItem>
+    <List sx={{ width: "100%", bgcolor: "background.paper" }}>
+      {cart?.map((item) => (
+        <ListItem
+          sx={{
+            ...(matches && {
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+              boxShadow: 3,
+              marginBottom: 5,
+            }),
+          }}
+          key={item.id}
+        >
+          <ListItemAvatar>
+            <Avatar>
+              <img className={styles.img} src={item.logo_img} alt={item.name} />
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText
+            sx={{
+              ...(!matches && { width: 280 }),
+            }}
+          >
+            {item.name}
+          </ListItemText>
+          <ListItemText sx={{ width: 218 }}>
+            <Typography>Valor: </Typography>
+            <Typography fontWeight={500}>
+              {item.price.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </Typography>
+          </ListItemText>
+          <ListItemText sx={{ width: 218 }}>
+            <Typography>Total:</Typography>
+            <Typography fontWeight={500}>
+              {item.total_price.toLocaleString("pt-br", {
+                style: "currency",
+                currency: "BRL",
+              })}
+            </Typography>
+          </ListItemText>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+        
+            }}
+          >
+            <ListItemButton
+              sx={{ justifyContent: "center" }}
+              component="button"
+              onClick={() => handleQtdRemove(item.id)}
+            >
+              <RemoveCircleIcon />
+            </ListItemButton>
+            <ListItemText sx={{ width: 10 }}>
+              <Typography textAlign={"center"}>{item.qtd}</Typography>
+            </ListItemText>
+
+            <ListItemButton
+              sx={{ justifyContent: "center" }}
+              component="button"
+              onClick={() => handleQtdAdd(item.id)}
+            >
+              <AddCircleIcon />
+            </ListItemButton>
+
+            <ListItemButton>
+              <IconButton
+                edge="start"
+                aria-label="delete"
+                onClick={() => {
+                  handleRemover(item);
+                }}
+              >
+                <DeleteIcon />
+              </IconButton>
+            </ListItemButton>
+          </Box>
+        </ListItem>
+      ))}
     </List>
   );
 }
--- src/components/cart/view.tsx
+++ src/components/cart/view.tsx
@@ -1,12 +1,24 @@
 import ListProductsComponent from "@/components/cart/list";
+import styles from "./styles.module.css";
+import { useCart } from "@/shared/hook/cart";
+import { Divider, Typography } from "@mui/material";
 
 export const CartComponent = () => {
+  const { total } = useCart();
   return (
     <div>
       <ListProductsComponent />
-      <section >
-        
-
+      <Divider sx={{ mb: 5 }} />
+      <section className={styles.total_prices}>
+        <Typography variant="h4" fontWeight={500}>
+          Total:
+        </Typography>
+        <Typography variant="h4" fontWeight={700}>
+          {total.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </Typography>
       </section>
     </div>
   );
